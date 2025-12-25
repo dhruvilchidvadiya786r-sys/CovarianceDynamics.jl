64117761803a7c9ac4d33b8e15b24944da09a6c4
dhruvilchidvadiya786r-sys <dhruvilchidvadiya786r@gmail.com>
2025-12-25 10:22:32 +0530

    Update CovarianceDynamicsStatisticsExt.jl

--- src/extensions/CovarianceDynamicsStatisticsExt.jl
+++ src/extensions/CovarianceDynamicsStatisticsExt.jl
@@ -1,1 +1,10 @@
 
+module CovarianceDynamicsStatisticsExt
+
+using CovarianceDynamics
+using Statistics
+
+# Re-export Statistics functionality used by the package
+CovarianceDynamics._mean(x) = mean(x)
+
+end
